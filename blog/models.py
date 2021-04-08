0d6561af1a9569d3ef50c1422743f45be75b76af
Aman Bahuguna <amanbahuguna002@gmail.com>
2021-04-08 22:47:23 +0530

    Added searches functionality

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,35 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.db.models import Q
+
+
+class PostQuerySet(models.QuerySet):
+    def search(self, query):
+        lookup = Q(title__icontains=query) | Q(text__icontains=query)
+        return self.filter(lookup)
+
+
+class PostManager(models.Manager):
+    def get_queryset(self):
+        return PostQuerySet(self.model, using=self._db)
+
+    def search(self, query=None):
+        if query is None:
+            return self.get_queryset().none()
+        return self.get_queryset().search(query)
 
 
 class Post(
     models.Model
 ):  # Means that the Post is a Django Model, so Django knows that it should be saved in the database.
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+
+    objects = PostManager()
 
     def publish(self):
         self.published_date = timezone.now()
